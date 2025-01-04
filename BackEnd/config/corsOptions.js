1274eccdef0b64ae0eeb3a21c0513bb5f2a874da
Dinesh L <dineshlokesh0406@gmail.com>
2025-01-04 17:30:44 +0530

    Update corsOptions.js

--- BackEnd/config/corsOptions.js
+++ BackEnd/config/corsOptions.js
@@ -1,5 +1,5 @@
 
-const allowedOrigins = ['http://localhost:5173','http://localhost:5174']
+const allowedOrigins = ['http://localhost:5173','http://localhost:5174','https://restopenadmin.onrender.com']
 
 const corsOptions ={
     origin: (origin,callback)=>{
