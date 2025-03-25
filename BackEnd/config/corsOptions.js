fa0cde4e9d9319adfe13c95a1fd17825a58e02e8
Dinesh L <dineshlokesh0406@gmail.com>
2025-03-25 19:16:28 +0530

    commit

--- BackEnd/config/corsOptions.js
+++ BackEnd/config/corsOptions.js
@@ -1,16 +1,15 @@
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:5174'];
 
-const allowedOrigins = ['http://localhost:5173/','http://localhost:5174/']
-
-const corsOptions ={
-    origin: (origin,callback)=>{
-        if(allowedOrigins.indexOf(origin)!== -1 || !origin){
-            callback(null,true)
-        }else{
-            callback(new Error('Not allowed by CORS'))
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
         }
-   },
-    credentials:true,  //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-module.exports = corsOptions;
+    },
+    credentials: true, // access-control-allow-credentials:true
+    optionsSuccessStatus: 200 // Corrected the key name
+};
+
 module.exports = corsOptions;
